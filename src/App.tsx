83593239e064bf4c7b2ae301dc3808116abf824d
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-07-15 17:12:27 +0600

    22-5 Initializing Shadcn UI

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+  return (
+    <>
+      
+      <h1>Task Manager</h1>
+      <Button>Click Me</Button>
+        
+      
+     
+    </>
+  )
+}
+
+export default App
