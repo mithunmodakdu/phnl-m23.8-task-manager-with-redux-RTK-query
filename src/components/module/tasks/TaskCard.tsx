788009fe58d31496392415cb277a45d179cd9390
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-07-16 22:19:19 +0600

    23-4 Managing Task Completion and Deletion

--- src/components/module/tasks/TaskCard.tsx
+++ src/components/module/tasks/TaskCard.tsx
@@ -6,14 +6,20 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { Checkbox } from "@/components/ui/checkbox"
 import { cn } from "@/lib/utils"
+import { deleteTask, toggleCompleteState } from "@/redux/features/task/taskSlice"
+import { useAppDispatch } from "@/redux/hooks"
 import type { ITask } from "@/types"
+import { Trash2Icon } from "lucide-react"
 
 interface IProps{
   task: ITask
 }
 
 export function TaskCard({task} : IProps) {
+  const dispatch = useAppDispatch();
+
   return (
     <Card className={cn("w-full", {
       "bg-green-500": task.priority === "Low",
@@ -21,12 +27,15 @@ export function TaskCard({task} : IProps) {
       "bg-red-500": task.priority === "High"
     })}>
       <CardHeader>
-        <CardTitle>{task.title}</CardTitle>
-        <CardDescription>
+        <CardTitle className={cn({"line-through": task.isCompleted})}>{task.title}</CardTitle>
+        <CardDescription className={cn({"line-through": task.isCompleted})}>
           {task.description}
         </CardDescription>
-        <CardAction>
-          <Button variant="link">Delete</Button>
+        <CardAction className="flex justify-center items-center gap-5">
+          <Button variant="link" onClick={() => dispatch(deleteTask(task.id))}>
+            <Trash2Icon className="ml-auto h-4 w-4 opacity-50" />
+          </Button>
+          <Checkbox checked={task.isCompleted} onClick={() => dispatch(toggleCompleteState(task.id))} />
         </CardAction>
       </CardHeader>
       
--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -8,44 +8,66 @@ interface IInitialState {
   filter: "all" | "high" | "medium" | "low";
 }
 
-const initialState : IInitialState = {
-  tasks: [ ],
-  filter: "all"
+const initialState: IInitialState = {
+  tasks: [
+    {
+      id: "G1imG5rU2Gk9DCqw4bMmm",
+      isCompleted: false,
+      title: "JavaScript",
+      description:
+        "JavaScriptJavaScriptJavaScriptJavaScriptJavaScriptJavaScriptJavaScriptvJavaScriptJavaScript",
+      priority: "High",
+      dueDate: "2025-07-16T18:00:00.000Z",
+    },
+  ],
+  filter: "all",
 };
 
 type DraftTask = Pick<ITask, "title" | "description" | "priority" | "dueDate">;
 
-const createTaskData = (formTaskData: DraftTask) : ITask =>{
-  return {id: nanoid(), isCompleted: false, ...formTaskData}
-}
+const createTaskData = (formTaskData: DraftTask): ITask => {
+  return { id: nanoid(), isCompleted: false, ...formTaskData };
+};
 
 const taskSlice = createSlice({
   name: "task",
   initialState,
   reducers: {
-    addTask: (state, action: PayloadAction<ITask>) =>{
-      
+    addTask: (state, action: PayloadAction<ITask>) => {
       // const id = uuidv4();
-      
+
       // const taskData = {
       //   ...action.payload,
       //   id,
       //   isCompleted: false
       // }
 
-      const taskData = createTaskData(action.payload)
+      const taskData = createTaskData(action.payload);
       state.tasks.push(taskData);
+    },
+
+    toggleCompleteState: (state, action: PayloadAction<string>) => {
+      console.log(action)
+      state.tasks.forEach((task) =>
+        task.id === action.payload
+          ? (task.isCompleted = !task.isCompleted)
+          : task
+      );
+    },
+
+    deleteTask: (state, action: PayloadAction<string>) => {
+      state.tasks = state.tasks.filter((task) => task.id !== action.payload)
     }
   },
 });
 
-export const selectTasks = (state: RootState) =>{
+export const selectTasks = (state: RootState) => {
   return state.todo.tasks;
-}
+};
 
-export const selectFilter = (state: RootState) =>{
+export const selectFilter = (state: RootState) => {
   return state.todo.filter;
-}
+};
 
-export const {addTask} = taskSlice.actions;
+export const { addTask, toggleCompleteState, deleteTask } = taskSlice.actions;
 export default taskSlice.reducer;
