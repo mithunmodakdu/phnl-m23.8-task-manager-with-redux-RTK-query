db08f0e7c771d69174aab4a010924507e129c0d0
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-07-20 15:23:52 +0600

    first commit

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
-import taskReducer from "./features/task/taskSlice";
-import userReducer from "./features/user/userSlice"
+// import { configureStore } from "@reduxjs/toolkit";
+// import taskReducer from "./features/task/taskSlice";
+// import userReducer from "./features/user/userSlice"
 
-export const store = configureStore({
-  reducer: {
-    todo: taskReducer,
-    users: userReducer
-  }
-})
+// export const store = configureStore({
+//   reducer: {
+//     todo: taskReducer,
+//     users: userReducer
+//   }
+// })
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
